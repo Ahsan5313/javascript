5c52b0e7d8e415137d453924f557259f2b3de336
Ahsan5313 <81156921+Ahsan5313@users.noreply.github.com>
2021-04-07 09:11:01 +0600

    add javascript and html for the project

---
+++ index.js
@@ -0,0 +1,19 @@
+var nam=prompt("Enter your first name : ");
+var nam1=prompt("Enter your last name : ");
+
+var nam2=(nam+nam1);
+ var nam3(nam2.length);
+var nam4=(nam2.toUpperCase());
+document.write(nam4.charAt(2));
+
+
+
+
+
+
+
+
+
+
+
+
